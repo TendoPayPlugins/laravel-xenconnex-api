d02767a354cf765c2584be0ddfbf726d9e4d325f
adam <adam.jakomulski@gmail.com>
2022-03-12 14:56:38 +0100

    Backbone structure

---
+++ src/TendoPay/Integration/XenConnex/XenConnexServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace TendoPay\Integration\XenConnex;
+
+use Illuminate\Support\ServiceProvider;
+
+class XenConnexServiceProvider extends ServiceProvider
+{
+    /**
+     * Loads the configuration file.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
